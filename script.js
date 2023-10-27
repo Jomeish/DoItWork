954edee372331a43ffa2f5c0e943721dd9c2bd77
jomeis <jomeishes@gmail.com>
2023-10-26 21:30:13 -0700

    hehejs does actually work

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const leftNav = document.getElementById('leftNav');
-
-leftNav.addEventListener('mouseover', function() {
-    leftNav.style.backgroundColor = 'white';
-    leftNav.classList.remove('col-1');
-    leftNav.classList.add('col-2');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const leftNav = document.getElementById('leftNav');
+  
+    leftNav.addEventListener('mouseover', function() {
+        // leftNav.style.backgroundColor = 'white';
+        leftNav.classList.add('col-md-2');
+        leftNav.classList.remove('col-md-1');
+    //   console.log('Mouse over leftNav');
+    });
+    leftNav.addEventListener('mouseout', function() {
+        // leftNav.style.backgroundColor = 'transparent';
+        leftNav.classList.add('col-md-1');
+        leftNav.classList.remove('col-md-2');
+    //   console.log('Mouse out leftNav');
+    });
+  });
